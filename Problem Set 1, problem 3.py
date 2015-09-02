9a04da631ba001a21703e5f61ed19455b55189c6
Christopher Jones <jones.christopherd@gmail.com>
2015-09-01 21:45:54 -0400

    Update and rename Problem Set 1, problem 3 to Problem Set 1, problem 3.py

--- Problem Set 1, problem 3.py
+++ Problem Set 1, problem 3.py
@@ -4,7 +4,7 @@ import winsound
 Freq = 250 # Set Frequency To 2500 Hertz
 Dur = 200 # Set Duration To 1000 ms == 1 second
 ############################
-s = 'jgpapwhpdlkdagit'
+s = 'xlojrhmdwvsoufiskpynyf'
 
 i = 0
 length = len(s)
@@ -14,18 +14,19 @@ longest = ''
 for i in range(length):
 	temp = ''
 	j = 0
-	for j in range(length - 1):
-		if s[j] > s[j + 1]:
+	for j in range(length):
+		if s[j] == s[-1]:
 			temp += s[j]
 			if len(temp) > len(longest):
 				longest = temp
 			temp = ''
-			#j += 1
+		elif s[j] > s[j + 1]:
+			temp += s[j]
+			if len(temp) > len(longest):
+				longest = temp
+			temp = ''
 		else:
 			temp += s[j]
-			#j += 1
-			#print temp
-	#i += 1
 	
 print "Longest substring in alphabetical order is:" + longest
 	
