8f4acce9de00c829a00c82eabbef170910b1d964
Christopher Jones <jones.christopherd@gmail.com>
2015-09-01 21:53:41 -0400

    Update and rename Problem Set 2, problem 2 to Problem Set 2, problem 2.py

--- Problem Set 2, problem 2.py
+++ Problem Set 2, problem 2.py
@@ -1,16 +1,17 @@
-balance = 4773
-annualInterestRate = 0.2
+#balance = 4773
+#annualInterestRate = 0.2
 monthlyInterestRate = annualInterestRate / 12.0
-pay = 10
+pay = 0
 remBalance = balance
 
 while remBalance > 0:
     month = 0
     remBalance = balance
+    pay += 10
     while month < 12 and remBalance > 0:
         month += 1
         remBalance -= pay
         remBalance += monthlyInterestRate * remBalance
-    pay += 10
+
 
 print "Lowest Payment: ", pay
